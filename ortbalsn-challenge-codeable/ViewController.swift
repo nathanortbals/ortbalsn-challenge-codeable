db584d0432092e5e0f4c40d366b038350d499d5b
Nathan Ortbals <nathan.ortbals@gmail.com>
2019-04-26 11:13:33 -0500

    Initial Commit

---
+++ ortbalsn-challenge-codeable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ortbalsn-challenge-codeable
+//
+//  Created by Nathan Ortbals on 4/26/19.
+//  Copyright © 2019 nathanortbals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
